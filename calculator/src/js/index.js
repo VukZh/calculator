b8fea2a0586dcb060259ce2dd958920f434e17f7
VukZh <vukfx@inbox.ru>
2020-02-06 23:02:52 +0600

    feat: add basic page layout and webpack

---
+++ calculator/src/js/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars */
+
+const css = require('../style.styl');
---
+++ calculator/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+
+  entry: './src/js/index.js',
+
+  //  output: //default
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+
+  devtool: 'source-map', 
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false
+            },
+         }
+        ]
+      },
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/images/[name].[ext]'
+            },
+         }
+        ]
+      },      
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: MiniCssExtractPlugin.loader // translates CSS into CommonJS
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "stylus-loader" // compiles Stylus to CSS
+          }
+        ]
+      }
+    ]
+  }
+
+
+
+}
